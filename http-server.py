f308e75c264bd34916222ad7750457664b6ed261
40923123 <40923123@gm.nfu.edu.tw>
2020-11-24 11:26:13 +0800

    cp2020ag1/11/24

--- http-server.py
+++ http-server.py
@@ -6,7 +6,7 @@ import http.server, ssl
 def domake():
     # build directory
     #os.chdir("./../")
-    server_address = ('localhost', 8444)
+    server_address = ('localhost', 8442)
     httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
     httpd.socket = ssl.wrap_socket(httpd.socket,
                                    server_side=True,
@@ -14,7 +14,7 @@ def domake():
                                    keyfile='./cmsimde/localhost.key',
                                    ssl_version=ssl.PROTOCOL_TLSv1)
     print(os.getcwd())
-    print("8444 https server started")
+    print("8442 https server started")
     httpd.serve_forever()
 
 # 利用執行緒執行 https 伺服器
